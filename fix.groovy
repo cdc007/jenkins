23deb43ae99b6f24fd08a670f01b38e30d5082f9
Frank Chen <dacheng_chen@yahoo.com>
2021-07-04 21:42:48 -0700

    add by fix

---
+++ fix.groovy
@@ -0,0 +1,17 @@
+node{
+
+    stage('SCM'){
+        echo 'gather git change......'
+    }
+
+    stage('Build'){
+        echo 'building..........'
+    }
+    stage('Test'){
+        echo 'testing..........'
+    }
+    stage('Deploy'){
+        echo 'deploy...........'
+    }
+
+}
